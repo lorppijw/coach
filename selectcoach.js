1729f8a1eebcfdfbee75d9a228a0911137d29e43
Jon Wessman <lorppi@Jon-MacBook-Air.local>
2022-11-16 00:12:02 +0200

    added responsive text and button to coach select

--- selectcoach.js
+++ selectcoach.js
@@ -9,30 +9,38 @@ function pageFullyLoaded(e) {
     bull = document.getElementById("bull")
     pt = document.getElementById("pt")
     tate = document.getElementById("tate")
-    bull.addEventListener("click", setGolf);
-    pt.addEventListener("click", setViolin);
-    tate.addEventListener("click", setFootball);
+    bull.addEventListener("click", setBull);
+    pt.addEventListener("click", setVatanen);
+    tate.addEventListener("click", setTate);
 }
 
-function setGolf(){
+function setBull(){
     resetAll();
     bull.style.border = "2px solid white"
     bull.style.borderRadius = "5px"
     selection = "golf"
+    document.getElementById("coachName").textContent = "Bull Mentula"
+    document.getElementById("coachDescription").textContent = "Varma valinta kokeneemmille harrastajille erityisesti ekspressionistiseen taidemaalaukseen ja itämaiseen huilunsoittoon."
+
 }
 
-function setViolin(){
+function setVatanen(){
     resetAll();
     pt.style.border = "2px solid white"
     pt.style.borderRadius = "5px"
     selection = "violin"
+    document.getElementById("coachName").textContent = "PT Vatanen"
+    document.getElementById("coachDescription").textContent = "Kova äijä bodaa. Lambo löytyy."
+    
 }
 
-function setFootball(){
+function setTate(){
     resetAll();
     tate.style.border = "2px solid white"
     tate.style.borderRadius = "5px"
     selection = "football"
+    document.getElementById("coachName").textContent = "Andrew Tate"
+    document.getElementById("coachDescription").textContent = "Top G"
 }
 
 function resetAll(){
