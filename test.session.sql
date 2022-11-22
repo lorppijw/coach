a7c35a233616e78c28bbee7f605c4043c797a08b
kfjowe <jon.wessman@tuni.fi>
2022-11-22 22:53:24 +0200

    started work on node server

---
+++ test.session.sql
@@ -0,0 +1,30 @@
+
+-- @block
+CREATE TABLE Users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    passwd VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phoneNumber VARCHAR(255) NOT NULL UNIQUE,
+    age INT NOT NULL,
+    sport VARCHAR(255) NOT NULL
+);
+
+-- @block
+INSERT INTO users (username, passwd, email, phoneNumber, age, sport)
+VALUES (
+    'lorppi',
+    'sipuli609',
+    'lorppi@coachcoach.com',
+    '0445091101',
+    '22',
+    'football'
+)
+
+-- @block
+SELECT username, id FROM Users
+
+WHERE age = '22'
+
+ORDER BY id ASC
+LIMIT 100;
